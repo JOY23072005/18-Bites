092333e478445823c48e9eb83d1700d8a4defcc6
JOY23072005 <hansjoydeep@gmail.com>
2026-02-01 13:07:30 +0530

    bug fix

--- Backend/src/routes/home.routes.js
+++ Backend/src/routes/home.routes.js
@@ -2,11 +2,11 @@ import express from "express"
 import { authMiddleware } from "../middlewares/auth.middleware";
 import { adminMiddleware } from "../middlewares/admin.middleware";
 import { uploadBannerImages } from "../middlewares/upload.middleware";
-import { addHomeBanner, updateHomeVideo } from "../controllers/home.controller";
+import { addHomeBanner, getHomeConfig, updateHomeVideo } from "../controllers/home.controller";
 
 const HomeRoutes = express.Router();
 
-HomeRoutes.get("/", getHomeBanner);
+HomeRoutes.get("/", getHomeConfig);
 HomeRoutes.post("/", authMiddleware, adminMiddleware,uploadBannerImages,addHomeBanner );
 HomeRoutes.put("/video", authMiddleware, adminMiddleware, updateHomeVideo);
 
